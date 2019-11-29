4ff248cc9ab994e32c8d6162c485dec0b90de4b5
Nando Vieira <me@fnando.com>
2019-11-29 13:49:36 -0800

    Prefer overrides. Also, add everything as direct dependency.

--- typescript.js
+++ typescript.js
@@ -1,39 +1,49 @@
+const path = require("path");
+
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:import/typescript",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  settings: {
-    "import/resolver": {
-      node: {
-        extensions: [".ts", ".tsx", ".json"],
+  overrides: [
+    {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: path.join(process.cwd(), "tsconfig.json")
       },
-    },
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "react/jsx-filename-extension": ["error", { extensions: ["tsx"] }],
-    "@typescript-eslint/consistent-type-assertions": "warn",
-    "@typescript-eslint/no-array-constructor": "error",
-    "@typescript-eslint/no-namespace": "error",
-    "@typescript-eslint/no-var-requires": "error",
-    "@typescript-eslint/no-use-before-define": [
-      "warn",
-      {
-        functions: false,
-        classes: false,
-        variables: false,
-        typedefs: false,
+      extends: [
+        "plugin:import/typescript",
+        "plugin:@typescript-eslint/recommended",
+      ],
+      settings: {
+        "import/resolver": {
+          node: {
+            extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+          },
+        },
       },
-    ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        args: "none",
-        ignoreRestSiblings: true,
-      },
-    ],
-    "@typescript-eslint/no-useless-constructor": "error",
-  }
+      plugins: ["@typescript-eslint"],
+      files: "**/*.{ts,tsx}",
+      rules: {
+        "react/jsx-filename-extension": ["error", { extensions: ["tsx"] }],
+        "@typescript-eslint/consistent-type-assertions": "warn",
+        "@typescript-eslint/no-array-constructor": "error",
+        "@typescript-eslint/no-namespace": "error",
+        "@typescript-eslint/no-var-requires": "error",
+        "@typescript-eslint/no-use-before-define": [
+          "warn",
+          {
+            functions: false,
+            classes: false,
+            variables: false,
+            typedefs: false,
+          },
+        ],
+        "@typescript-eslint/no-unused-vars": [
+          "warn",
+          {
+            args: "none",
+            ignoreRestSiblings: true,
+          },
+        ],
+        "@typescript-eslint/no-useless-constructor": "error",
+      }
+    }
+  ]
 }
